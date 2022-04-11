e809162616f872201043db1388f9077a90b65e0d
juanorts <91051695+juanorts@users.noreply.github.com>
2022-04-11 20:44:21 +0200

    Funciones BD

--- Deusto Bet/usuario.h
+++ Deusto Bet/usuario.h
@@ -1,8 +1,6 @@
 #ifndef USUARIO_H_
 #define USUARIO_H_
 #include <stdbool.h>
-#include <stdlib.h>
-
 
 typedef struct
 {
@@ -14,10 +12,4 @@ typedef struct
     bool admin;
 } Usuario;
 
-void registrarUsuario(char* username, char *contrasena, Usuario **usuarios, int numUsuarios);
-void registrarAdmin(char* username, char *contrasena, Usuario **usuarios, int numUsuarios);
-void modificarUsuario(char* username, char *contrasena, Usuario **usuarios, int index);
-void eliminarUsuario(Usuario **usuarios, int index, int numUsuarios);
-void imprimirUsuario(Usuario usuario);
-void imprimirUsuarios(Usuario *usuarios, int numUsuarios);
 #endif

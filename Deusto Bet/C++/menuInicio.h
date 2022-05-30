273869bb496e9695ded04b943b7c2733512294df
juanorts <91051695+juanorts@users.noreply.github.com>
2022-05-30 17:48:06 +0200

    Delete Deusto Bet/C++ directory

--- Deusto Bet/C++/menuInicio.h
+++
@@ -1,9 +0,0 @@
-#include "usuario.h"
-extern "C"
-{
-    #include "../C/baseDeDatos.h"
-}
-
-int menuInicio();
-int menuLogin(Usuario* usuarios, int numUsuarios);
-int menuRegistroUsuario(Usuario** usuarios, int numUsuarios);
